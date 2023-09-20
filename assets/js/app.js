04adae9ed50a0c3182db354ebdeb3906c07fb9c7
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-20 13:47:12 +0200

    add for cycle

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,29 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        emailsList : [],
+        list : [],        
+        email : '',
       }
+    },
+
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+
+            list = [],
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                console.log(response.data.response);
+                this.email = response.data.response;
+                list.push(this.email)
+                
+            })
+            
+        }
+        
+                       
+        console.log(list);
+
     }
   }).mount('#app')
